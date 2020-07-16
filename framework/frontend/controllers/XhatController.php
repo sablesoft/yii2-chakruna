d7e3878f2b2a4a9cda2db199c147801ef8150023
Roman <sable.lair@gmail.com>
2020-07-17 01:09:01 +0300

    add composer packages

--- framework/frontend/controllers/XhatController.php
+++ framework/frontend/controllers/XhatController.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: roan
- * Date: 29.11.19
- * Time: 19:57
- */
 
 namespace frontend\controllers;
 
@@ -12,25 +6,11 @@ use yii\web\Controller;
 
 class XhatController extends Controller
 {
-
-    public function actionIndex()
-    {
-
-    }
-
-    /**
-     *
-     */
     public function actionPull()
     {
         // ловим ключ запроса:
         $xhtKey = \Yii::$app->request->get('key');
         // возвращаем html:
         return $this->renderPartial("$xhtKey.html");
-    }
-
-    public function actionPush()
-    {
-
     }
 }
